507c8e50f9f03d498c9d99bf2e3a790e42f0d122
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2023-01-30 17:42:01 +0500

    Add files via upload

---
+++ twitter_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Twitter access codes:
+# API Key
+API_key = 'YOUR API KEY HERE'
+# API Key Secret
+API_key_secret = 'YOUR API KEY SECRET HERE'
+# Access Token
+Access_Token = 'YOUR ACCESS TOKEN HERE'
+# Access Token Secret
+Access_Token_secret = 'YOUR ACCESS TOKEN SECRET HERE'
